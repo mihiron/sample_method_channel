86f4ccef4e0b35c1db85be570a44ae47735c293c
mihiro <f.mihiroxx@gmail.com>
2023-03-24 15:13:35 +0900

    ベースの作成

---
+++ lib/src/routes/app_router.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:sample_method_channel/src/views/home_page.dart';
+
+import 'app_routes.dart';
+
+class AppRouter {
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.home:
+        return _buildRoute(const MyHomePage());
+
+      default:
+        return null;
+    }
+  }
+
+  static Route<dynamic>? _buildRoute(
+    Widget child, {
+    RouteSettings? settings,
+    bool fullscreenDialog = false,
+  }) {
+    return MaterialPageRoute<dynamic>(
+      builder: (BuildContext context) => child,
+      settings: settings,
+      fullscreenDialog: fullscreenDialog,
+    );
+  }
+}
